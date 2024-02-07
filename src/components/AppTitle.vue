033ed8bbb02351d60e9f2d8a7c547555c61247e0
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-07 15:30:35 +0100

    feat: add Apptitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con vite",
+    };
+  },
+};
+</script>
+<template>
+  <h1>
+    <h1 class="red">{{ title }}</h1>
+  </h1>
+</template>
+<style scoped>
+.red {
+  color: red;
+}
+</style>
